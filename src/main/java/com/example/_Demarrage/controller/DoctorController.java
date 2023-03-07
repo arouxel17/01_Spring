c4f5fdfaf177b075c9ee2b560194f80ed63d051d
arouxel17 <alexandrerouxel17@gmail.com>
2023-03-07 10:39:27 +0100

    correction

--- src/main/java/com/example/_Demarrage/controller/DoctorController.java
+++ src/main/java/com/example/_Demarrage/controller/DoctorController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DoctorController {
     @GetMapping("/doctor/1");
     @ResponseBody
-    public String index() {
+    public String index1() {
         return "William Hartnell";
     }
     
     @GetMapping("/doctor/10");
     @ResponseBody
-    public String index() {
+    public String index10() {
         return "David Tennant";
     }
 
     @GetMapping("/doctor/13");
     @ResponseBody
-    public String index() {
+    public String index13() {
         return "Jodie Whittaker";
     }
 }
